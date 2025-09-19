182614adb7fe565f0ce467db58808f8d418326a2
Renusri08 <n210111@rguktn.ac.in>
2025-09-19 18:42:16 +0000

    changed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -68,6 +68,8 @@ export const login =async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:24*60*60*1000,
+            secure:"true",
+            sameSite:"none"
         });
 
         return res.json({message:"login successful",success:true,user});
